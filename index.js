9fc2b500aacc1224dab08f0347aaebab148e1a9d
Jonathan Rosenbaum <yoni@Jonathans-MacBook-Air.local>
2020-04-17 20:23:33 +1000

    git second deployment attempt

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/', (req, res) => {
     8. Look at video 15 of the app to kow how to do all the command line
        setup.
 */}
+
+{/*             package.json
+// you need to do the below command "engines" to specify which npm
+  // and node versions to use on hiroku.
+
+// values in the udemy
+//"node": "8.1.1",
+//"npm": "5.0.3"
+// https://hidden-beyond-37232.herokuapp.com/ | https://git.heroku.com/hidden-beyond-37232.git
+*/}
 const PORT = process.env.PORT || 5000;
 // an answer in udemy said to provide the second argument '0.0.0.0' with heroku
 app.listen(PORT);
